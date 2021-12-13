b0adfc3499db66079165c837f88e93170ad28fa2
Kiet Nguyen <kietnguyen1999@pm.me>
2021-12-13 10:04:29 +0700

    simplifying code

--- src/main/java/com/huongdoituong/App.java
+++ src/main/java/com/huongdoituong/App.java
@@ -1,64 +1,40 @@
 package com.huongdoituong;
 
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private static final StringBuilder BUILDER = new StringBuilder();
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    private static final List<FuncTypes> menuMain = Arrays.asList(FuncTypes.THUESANH, FuncTypes.QUANLY);
-    private static final List<FuncTypes> menuQuanLy = Arrays.asList(FuncTypes.QUANLY_SANHCUOI,
-                                                                    FuncTypes.QUANLY_DICHVU,
-                                                                    FuncTypes.QUANLY_THUE,
-                                                                    FuncTypes.QUANLY_THUCAN,
-                                                                    FuncTypes.QUANLY_THUCUONG);
-
-    private static FuncTypes funcType = FuncTypes.NONE;
-
     public static void main(String[] args) {
-        ChooseFunc(menuMain, "Quan ly nha hang\n1. Thue\n2. Quan ly\nChon chuc nang: ");
-
-        ChooseFunc(menuQuanLy, "Quan ly \n1. Thue\n2. Quan ly\nChon chuc nang: ");
-    }
-
-    private static void ChooseFunc(List<FuncTypes> funcTypes, String guide) {
-        boolean isCheckWrongInput = false;
-        int funcSelection = 0;
-
-        while (!funcTypes.contains(funcType)) {
-            if (isCheckWrongInput && !funcTypes.contains(funcType)) {
-                System.out.print("***Chuc nang khong kha dung***\n");
+        while (true) {
+            System.out.println("Quan ly nha hang");
+            System.out.println("1. Thue sanh");
+            System.out.println("2. Quan ly");
+            System.out.println("3. Thoat");
+            String choice = SCANNER.nextLine();
+            switch (choice) {
+                case "1": {
+                    break;
+                }
+                case "2": {
+                    System.out.println("Quan ly");
+                    System.out.println("1. Thue");
+                    System.out.println("2. Sanh");
+                    System.out.println("3. Dich vu");
+                    System.out.println("4. Thuc an");
+                    System.out.println("3. Thuc uong");
+                    System.out.println("5. Thoat");
+                    choice = SCANNER.nextLine();
+                    
+                    break;
+                }
+                case "3": {
+                    return;
+                }
+                default:
+                    System.out.println("*** Lua chong khong kha dung ***");
+                    continue;
             }
-
-            System.out.print(guide);
-            funcSelection = SCANNER.nextInt();
-
-            Hashtable<Integer, FuncTypes> hashtable = CreateHashTableFromList(funcTypes);
-
-            if (hashtable.containsKey(funcSelection)) {
-                funcType = hashtable.get(funcSelection);
-            }
-
-            isCheckWrongInput = true;
         }
-    }
-
-    private static Hashtable<Integer, FuncTypes> CreateHashTableFromList(List<FuncTypes> funcTypes) {
-        if (!funcTypes.isEmpty()) {
-            Hashtable<Integer, FuncTypes> hashtable = new Hashtable<>();
-            int count = 1;
-
-            for (FuncTypes funcType : funcTypes) {
-                hashtable.put(count, funcType);
-                count++;
-            }
-
-            return hashtable;
-        }
-
-        return null;
     }
 }
